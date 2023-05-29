c77a42cc2f97bb9e5825554d4bd4bc251b342c72
Kewon <yang0184@naver.com>
2023-05-30 02:29:26 +0900

    index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,3 @@
-<!-- /index -->
-
-<template>
-  <div>
-    <h2>Home</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid autem blanditiis, deserunt dolorem doloribus eius, facere iste, libero molestiae nobis odio omnis provident quam quod ut voluptate! Quasi, tempore!</p>
-    x: {{x}} y: {{y}}
-
-    <Input v-model="x" />
-  </div>
-</template>
-
 <script setup lang="ts">
 
 /******** Type & Interface **********/
@@ -21,24 +9,20 @@
 /******** Reactive Instance **********/
 
 
-const {x, y} = useMouse()
-
-
 /******** Hooks **********/
 
+definePageMeta({
+  redirect: () => {
+    // TODO 인증객체가 없으면 login Page Redirect
+    return '/auth/login';
+  },
+
+})
 
 /******** Functions **********/
-
 
 </script>
 
 <style scoped>
-  h2 {
-    margin-bottom: 20px;
-    font-size: 36px;
-  }
 
-  p {
-    margin: 20px 0;
-  }
 </style>
